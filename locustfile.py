7624108ea9d90f6c9ae92f08864a89feb91f8d55
yigitcan <yigitcan@users.noreply.github.com>
2020-04-15 14:15:44 +0200

    Point to the existing image file

--- locustfile.py
+++ locustfile.py
@@ -4,7 +4,7 @@ from locust import HttpLocust, TaskSet, task
 class UserBehavior(TaskSet):
     @task
     def predict(self):
-        with open('jemma.png', 'rb') as image:
+        with open('doge.jpg', 'rb') as image:
             self.client.post('/predict', files={'img_file': image})
 
 
